c23e7a47670b04fab799c8722fbfed9363a44262
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2017-12-06 16:23:43 +0000

    Trigger a webpagetest of ft.com every 12 hours  🐿 v2.5.16

--- functions/trigger-webpagetest.js
+++ functions/trigger-webpagetest.js
@@ -3,10 +3,11 @@ const fetch = require('node-fetch');
 const pingbackUrl='https://twist.in.ft.com/receive-webpagetest';
 
 const processRequest = request => {
+	const testUrl = request.queryStringParameters.url || 'https://www.ft.com'
 	const notificationEmail = request.queryStringParameters.email || 'adam.braimbridge@ft.com';
 	const webpagetestUrl = [
 		'https://www.webpagetest.org/runtest.php?',
-		`url=${request.queryStringParameters.url}`,
+		`url=${testUrl}`,
 		`pingback=${pingbackUrl}`,
 		`k=${process.env.WEBPAGETEST_API_KEY}`,
 		`notify=${notificationEmail}`,
